839dd60009dff643315b57399a15efa9d2242a0b
Justus Magin <keewis@posteo.de>
2026-04-26 21:12:34 +0200

    refactor to contain all connections in the same object

--- slurm_client/connection.py
+++ slurm_client/connection.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from typing import ClassVar, Self
 
 import asyncssh
+import httpx
 
 
 @dataclass
@@ -33,20 +34,7 @@ class Token:
 
 
 @dataclass
-class SocksProxy:
-    handle: asyncssh.SSHListener
-
-    def to_url(self):
-        return f"http://localhost:{self.handle.get_port()}"
-
-    async def close(self):
-        self.handle.close()
-
-        await self.handle.wait_closed()
-
-
-@dataclass
-class Connection:
+class SSHConnection:
     handle: asyncssh.SSHClientConnection
 
     async def close(self):
@@ -54,22 +42,48 @@ class Connection:
         await self.handle.wait_closed()
 
 
+@dataclass
+class SocksProxy:
+    listener: asyncssh.SSHListener
+
+    def to_url(self):
+        return f"http://localhost:{self.listener.get_port()}"
+
+    async def close(self):
+        self.listener.close()
+
+        await self.listener.wait_closed()
+
+
+@dataclass
+class Connection:
+    ssh: asyncssh.SSHClientConnection
+    socks_proxy: SocksProxy
+    api: httpx.AsyncClient
+
+    async def close(self):
+        await self.api.aclose()
+        await self.socks_proxy.close()
+        await self.ssh.close()
+
+    async def refresh_token(self, lifespan: dt.timedelta) -> Token:
+        now = dt.datetime.now(tz=dt.UTC)
+
+        result = await self.ssh.handle.run(
+            f"scontrol token lifespan={int(lifespan.total_seconds())}", check=True
+        )
+        return Token.from_expr(result.stdout.strip(), now + lifespan)
+
+
+async def create_socks_proxy(con: SSHConnection) -> SocksProxy:
+    listener = await con.handle.forward_socks("127.0.0.1", 0)
+
+    return SocksProxy(listener)
+
+
 async def connect(server: str) -> Connection:
-    con = await asyncssh.connect(server)
+    ssh = SSHConnection(await asyncssh.connect(server))
+    socks_proxy = await create_socks_proxy(ssh)
+    api = httpx.AsyncClient()
 
-    return Connection(con)
-
-
-async def refresh_token(con: Connection, lifespan: dt.timedelta) -> Token:
-    now = dt.datetime.now(tz=dt.UTC)
-
-    result = await con.handle.run(
-        f"scontrol token lifespan={int(lifespan.total_seconds())}", check=True
-    )
-    return Token.from_expr(result.stdout.strip(), now + lifespan)
-
-
-async def create_socks_proxy(con: Connection) -> SocksProxy:
-    handle = await con.handle.forward_socks("localhost", 0)
-
-    return SocksProxy(handle)
+    return Connection(ssh, socks_proxy, api)
