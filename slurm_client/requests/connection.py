f1db08448bb329477954defdda4f32466e7be45f
Justus Magin <keewis@posteo.de>
2026-04-28 22:30:09 +0200

    move the token and connection functions / classes into separate modules

--- slurm_client/requests/connection.py
+++ slurm_client/requests/connection.py
@@ -1,36 +1,10 @@
 import datetime as dt
-import re
 from dataclasses import dataclass
-from typing import ClassVar, Self
 
 import asyncssh
 import httpx
 
-
-@dataclass
-class Token:
-    token: str
-    valid_until: dt.datetime
-
-    token_expr_re: ClassVar[re.Pattern] = re.compile(r"SLURM_JWT=(.+)")
-
-    @classmethod
-    def from_expr(cls, expr: str, valid_until: dt.datetime) -> Self:
-        match = cls.token_expr_re.fullmatch(expr)
-        if match is None:
-            raise ValueError(f"invalid token expression: {expr}")
-
-        token = match.group(1)
-
-        return cls(token, valid_until)
-
-    def is_valid(self):
-        now = dt.datetime.now(tz=dt.UTC)
-
-        return now + dt.timedelta(seconds=1) < self.valid_until
-
-    def __str__(self):
-        return self.token
+from slurm_client.requests.token import Token
 
 
 @dataclass
@@ -66,14 +40,6 @@ class Connection:
         await self.socks_proxy.close()
         await self.ssh.close()
 
-    async def refresh_token(self, lifespan: dt.timedelta) -> Token:
-        now = dt.datetime.now(tz=dt.UTC)
-
-        result = await self.ssh.handle.run(
-            f"scontrol token lifespan={int(lifespan.total_seconds())}", check=True
-        )
-        return Token.from_expr(result.stdout.strip(), now + lifespan)
-
 
 async def create_socks_proxy(con: SSHConnection) -> SocksProxy:
     listener = await con.handle.forward_socks("127.0.0.1", 0)
@@ -87,3 +53,12 @@ async def connect(server: str) -> Connection:
     api = httpx.AsyncClient()
 
     return Connection(ssh, socks_proxy, api)
+
+
+async def refresh_token(con: SSHConnection, lifespan: dt.timedelta) -> Token:
+    now = dt.datetime.now(tz=dt.UTC)
+
+    result = await con.handle.run(
+        f"scontrol token lifespan={int(lifespan.total_seconds())}", check=True
+    )
+    return Token.from_expr(result.stdout.strip(), now + lifespan)
