7ad8da1b12cbed1748a7d636a087f6579329cb06
Justus Magin <keewis@posteo.de>
2026-04-28 23:28:07 +0200

    reduce the code directly on the app

--- slurm_client/app.py
+++ slurm_client/app.py
@@ -1,48 +1,18 @@
-from dataclasses import asdict, dataclass
-from typing import Any, Literal
+from typing import Any
 
 import httpx
-from textual import on
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal
-from textual.message import Message
-from textual.widgets import Footer, Header, Label
+from textual.widgets import Header
 
-from slurm_client.connection import connect, create_socks_proxy, refresh_token
-
-
-def extract_api_version(api_paths: dict[str, Any]) -> str:
-    versions = {
-        path.lstrip("/").split("/")[1]
-        for path in api_paths["paths"]
-        if path.startswith("/slurm")
-    }
-
-    return max(versions)
-
-
-def process_ping_response(result: dict[str, Any]) -> dict[str, Any]:
-    if "pings" not in result or len(result["pings"]) == 0:
-        return None
-
-    p = result["pings"][0]
-    slurm_version = result["meta"]["slurm"]["release"]
-
-    return {"server": p["hostname"], "latency": p["latency"], "version": slurm_version}
+from slurm_client.api import api_version, ping
+from slurm_client.api.connection import connect, create_socks_proxy, refresh_token
+from slurm_client.rest_api.request import Request
+from slurm_client.widgets.footer import SlurmClientFooter
 
 
 class SlurmClient(App):
     TITLE = "jobqueue-monitor"
     CSS_PATH = "app.tcss"
-
-    @dataclass
-    class PingMessage(Message):
-        server: str
-        latency: float
-        version: str
-
-        def items(self):
-            yield from asdict(self).items()
 
     def __init__(self, config):
         super().__init__()
@@ -56,86 +26,52 @@ class SlurmClient(App):
     def compose(self) -> ComposeResult:
         yield Header()
 
-        with Horizontal(id="footer-area"):
-            with Horizontal(id="footer-inner"):
-                yield Footer()
-            yield Label(id="server-info")
+        yield SlurmClientFooter()
 
     async def determine_api_version(self):
-        response = await self.query_api(method="GET", path="/openapi/v3")
+        response = await self.query_api(api_version)
 
         if response.status_code != httpx.codes.OK:
             return None
 
-        return extract_api_version(response.json())
+        self.post_message(api_version.response_parser(response.json()))
 
     async def setup_connections(self) -> None:
         self.ssh_con = await connect(self.config.server)
         self.socks_proxy = await create_socks_proxy(self.ssh_con)
         self.api_con = httpx.AsyncClient()  # proxy=self.socks_proxy.to_url())
 
-        self.api_version = await self.determine_api_version()
-
+        await self.determine_api_version()
         await self.ping()
 
     async def on_load(self) -> None:
-        self.run_worker(self.setup_connections, exclusive=True)
-
         self.token = None
         self.api_version = None
+
+        self.run_worker(self.setup_connections(), exclusive=True)
 
     async def on_mount(self) -> None:
         self.set_interval(self.config.ping_interval, self.ping)
 
     async def ping(self) -> str:
-        r = await self.query_api(method="GET", path="/slurm/{version}/ping")
+        r = await self.query_api(request=ping)
         if r.status_code != httpx.codes.OK:
-            server_info = None
+            server_info = {}
         else:
-            server_info = process_ping_response(r.json())
+            server_info = r.json()
 
-        if server_info is None:
-            server_info = {
-                "server": "unknown",
-                "latency": float("nan"),
-                "version": "n/a",
-            }
-
-        self.post_message(self.PingMessage(**server_info))
-
-    @on(PingMessage)
-    async def display_ping(self, message: PingMessage):
-        formatters = {
-            "latency": lambda x: f"{x / 1000:.3f} ms",
-        }
-        translations = {
-            "server": "name",
-        }
-
-        translated = {
-            translations.get(key, key): formatters.get(key, lambda x: x)(value)
-            for key, value in message.items()
-        }
-
-        formatted = "[b]server[/b]: " + " | ".join(
-            [f"[b]{k}[/b]: {v}" for k, v in translated.items()]
-        )
-
-        label = self.query_one("Label#server-info")
-        label.update(formatted)
+        footer = self.screen.query_one(SlurmClientFooter)
+        footer.post_message(ping.response_parser(server_info))
 
     async def query_api(
         self,
-        method: Literal["GET", "POST"],
-        path: str,
-        parameters: dict[str, Any] = None,
+        request: Request,
     ) -> dict[str, Any]:
-        if "{version}" in path:
+        if "{version}" in request.path:
             # fill in the api version
-            path = path.format(version=self.api_version)
-
-        if parameters is None:
-            parameters = {}
+            path = request.path.format(version=self.api_version)
+        else:
+            path = request.path
 
         if self.token is None or not self.token.is_valid():
             self.token = await refresh_token(
@@ -144,16 +80,14 @@ class SlurmClient(App):
 
         url = f"{self.config.address}/{path.lstrip('/')}"
 
-        methods = {
-            "POST": self.api_con.post,
-            "GET": self.api_con.get,
-        }
-        fetch = methods.get(method)
+        fetch = getattr(self.api_con, request.method, None)
         if fetch is None:
-            raise ValueError(f"invalid method: {method}")
+            raise ValueError(f"invalid method: {request.method}")
 
         return await fetch(
-            url, params=parameters, headers={"X-SLURM-USER-TOKEN": str(self.token)}
+            url,
+            params=request.parameters,
+            headers={"X-SLURM-USER-TOKEN": str(self.token)},
         )
 
     async def on_unmount(self) -> None:
