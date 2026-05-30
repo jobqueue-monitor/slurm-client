ba25e1bfbeb20f3db367c0ae711c81b3e0d915cd
Justus Magin <keewis@posteo.de>
2026-05-30 18:46:39 +0200

    add a parser for datetimes

--- slurm_client/rest_api/parsers.py
+++ slurm_client/rest_api/parsers.py
@@ -1,4 +1,5 @@
-from typing import TypedDict
+import datetime as dt
+from typing import Literal, TypedDict
 
 Infinite = object()
 
@@ -9,10 +10,19 @@ class ValueSet(TypedDict):
     number: int
 
 
-def parse_value_set(x: ValueSet) -> int | None:
+def parse_value_set(x: ValueSet) -> int | None | Literal[Infinite]:
     if not x["set"]:
         return None
     if x["infinite"]:
         return Infinite
 
     return x["number"]
+
+
+def parse_datetime(x: ValueSet) -> dt.datetime:
+    value = parse_value_set(x)
+
+    if not isinstance(value, int):
+        value = 0
+
+    return dt.datetime.fromtimestamp(value, dt.UTC)
