baa48c4701574b75d43711c6dcd9aad36076c90f
Justus Magin <keewis@posteo.de>
2026-05-30 18:27:46 +0200

    parser for gres strings

--- slurm_client/rest_api/resources.py
+++ slurm_client/rest_api/resources.py
@@ -1,14 +1,33 @@
 import re
-from typing import TypedDict
+from typing import Any, NotRequired, TypedDict
 
 value_re = re.compile(r"(?P<value>[0-9]+)(?P<units>[a-zA-Z]+)?")
 resource_re = re.compile(r"(?P<key>[-a-z0-9_:/]+)=(?P<value>[0-9M]+)")
+generic_resource_re = re.compile(
+    r"""
+    (?P<type>[a-z]+)
+    :(?P<name>[^:]+)
+    :(?P<quantity>[0-9]+)
+    (?:
+      \(
+      (?P<modifier_code>[A-Z]+)
+      :(?P<modifier_value>[-0-9]+|N/A)
+      \)
+    )?
+    """,
+    re.X,
+)
 
 
 class ResourceDict(TypedDict):
     cpu: str
     memory: str
     node: str
+
+
+class GenericResourceDict(TypedDict):
+    # no required members
+    pass
 
 
 default_resources: ResourceDict = {
@@ -23,6 +42,13 @@ translations = {"mem": "memory"}
 class ResourcesDict(TypedDict):
     total: ResourceDict
     used: ResourceDict
+
+
+class GenericResourcesDict(TypedDict):
+    total: GenericResourceDict
+    used: GenericResourceDict
+
+    drained: NotRequired[GenericResourceDict]
 
 
 def split_value(value: str | None) -> (int, str | None):
@@ -53,3 +79,39 @@ def parse_resources(total: str, used: str) -> ResourcesDict:
         "total": default_resources | parse_resource_spec(total),
         "used": default_resources | parse_resource_spec(used),
     }
+
+
+def noop(x: Any) -> Any:
+    return x
+
+
+def extract_resource_group(match) -> dict[str, Any]:
+    translations = {"S": "socket_affinity", "IDX": "index"}
+    converters = {"quantity": int}
+
+    modifier_code = match.group("modifier_code")
+    if modifier_code is not None:
+        modifier_value = match.group("modifier_value")
+        if modifier_value == "N/A":
+            value = []
+        elif "-" in modifier_value:
+            start, stop = map(int, modifier_value.split("-"))
+            value = list(range(start, stop + 1))
+        else:
+            value = [int(modifier_value)]
+        modifier = {translations[modifier_code]: value}
+    else:
+        modifier = {}
+
+    resource = {
+        name: converters.get(name, noop)(match.group(name))
+        for name in ["type", "name", "quantity"]
+    }
+
+    return resource | modifier
+
+
+def parse_generic_resource_spec(spec: str) -> list[GenericResourceDict]:
+    return [
+        extract_resource_group(match) for match in generic_resource_re.finditer(spec)
+    ]
